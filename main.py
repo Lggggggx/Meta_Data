552698ade1915f8b1141796a0af1b521573f73f0
BlackroseLi <3123652@qq.com>
2019-01-19 11:56:05 +0800

    initial label rate

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ dataset_path = './newdata/'
 datasetnames = np.load('datasetname.npy')
 # datasetnames = ['echocardiogram']
 # Different types of models, each type has many models with different parameters
-modelnames = ['KNN', 'LR', 'RFC', 'RFR', 'DTC', 'DTR', 'SVM', 'GBC', 'ABC', 'ABR']
-# modelnames = ['DTC', 'DTR', 'SVM']
+# modelnames = ['KNN', 'LR', 'RFC', 'RFR', 'DTC', 'DTR', 'SVM', 'GBC', 'ABC', 'ABR']
+modelnames = ['DTC', 'DTR', 'SVM']
 
 # in the same dataset and the same ratio of initial_label_rate,the number of split.
 split_count = 10
 # The number of unlabel data to select to generate the meta data.
-num_xjselect = 20
+num_xjselect = 2
 
 # first choose a dataset
 for datasetname in datasetnames:
@@ -40,7 +40,7 @@ for datasetname in datasetnames:
             else:
                 metadata = np.vstack((metadata, meta_data))
     else:
-        for i_l_r in np.arange(0.03, 0.08, 0.01, dtype=float):
+        for i_l_r in np.arange(0.03, 0.07, 0.02, dtype=float):
             # trains, tests, label_inds, unlabel_inds = dataset.split_data_labelbalance(test_ratio=0.3, 
             #     initial_label_rate=i_l_r, split_count=split_count, saving_path='./split')
             trains, tests, label_inds, unlabel_inds = dataset.split_load(path='./split_info',
